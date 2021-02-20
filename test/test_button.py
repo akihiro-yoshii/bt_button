d8a9322d595540b69ad6574a882811d1a3541754
Akihiro Yoshii <akihiro.441.r@gmail.com>
2021-02-20 16:12:21 +0900

    Add pytest and Github Actions setting

---
+++ test/test_button.py
@@ -0,0 +1,13 @@
+import pytest
+from bt_button.button import AbShutter
+
+@pytest.fixture
+def ab_shutter(mocker):
+    mocker.patch("bt_button.button.open_device", return_value=1)
+    return AbShutter()
+
+def test_add_pushed_listener(mocker, ab_shutter):
+    func = mocker.Mock()
+    ab_shutter.add_pushed_listener(func)
+
+    assert len(ab_shutter.pushed_funcs) == 1
