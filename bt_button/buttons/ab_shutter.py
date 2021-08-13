99f2cf107e02b44f864ac413ad30e3a0f7f809d3
Akihiro Yoshii <akihiro.441.r@gmail.com>
2021-08-13 18:47:55 +0900

    Remove duplicate function from AbShutter

--- bt_button/buttons/ab_shutter.py
+++ bt_button/buttons/ab_shutter.py
@@ -1,10 +1,7 @@
 import logging
-import threading
-
-import evdev
 from enum import Enum
 
-from ._device_manager import open_device, remove_device
+from ._event_device import EventDevice
 
 log = logging.getLogger(__name__)
 
@@ -20,7 +17,7 @@ class AbShutterButton(Enum):
     SMALL = 28
 
 
-class AbShutter:
+class AbShutter(EventDevice):
     """
     Create instance of AbShutter.
 
@@ -30,10 +27,7 @@ class AbShutter:
         AbShutter's MAC Address
     """
     def __init__(self, mac_addr):
-        self.mac_addr = mac_addr
-        self.device = None
-
-        self.name = "AB Shutter3"
+        super().__init__(mac_addr, "AB Shutter3")
 
         self.button_event_funcs = {}
         for button in list(AbShutterButton):
@@ -43,28 +37,6 @@ class AbShutter:
                 self.button_event_funcs[button][event] = None
 
         log.info("{}: initialized".format(self.name))
-
-    def is_connected(self):
-        """
-        Returns True if device is connected
-        """
-        return self.device is not None
-
-    def connect(self):
-        """
-        Connect to device and start to listen button event
-        """
-        self.device = open_device(self.name, self.mac_addr)
-        log.info("{}: connected".format(self.name))
-
-        self.thread = threading.Thread(target=self._run)
-        self.thread.setDaemon(True)
-        self.thread.start()
-
-    def _disconnect(self):
-        remove_device(self.device.path)
-        self.device = None
-        log.info("{}: disconnected".format(self.name))
 
     def attach_button_event_listener(self, button, event, func):
         """
@@ -87,16 +59,6 @@ class AbShutter:
         Detach function that be called when button event happened.
         """
         self.button_event_funcs[button][event] = None
-
-    def _run(self):
-        try:
-            for e in self.device.read_loop():
-                log.debug(e)
-
-                if e.type == evdev.events.EV_KEY:
-                    self._key_event(e)
-        except OSError:
-            self._disconnect()
 
     def _key_event(self, e):
         button = AbShutterButton(e.code)
